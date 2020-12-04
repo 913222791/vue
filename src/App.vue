06ed8feedb0d6b3ba467c1b290a13edcebe86981
Your Name <you@example.com>
2020-12-04 20:13:40 +0800

    modify

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,27 @@
       <router-link to="/pageA/123/vue">pageA</router-link>
       <router-link to="/pageB">pageB</router-link>
       <router-link to="/pageC">pageC</router-link>
-      <Button>123</Button>
+      <Button type="danger" disabled>123</Button>
       <Button type="primary" :loading="true">加载中</Button>
+      <Link href="https://element.eleme.io" target="_blank">默认链接</Link>
+      <Link type="primary" >主要链接</Link>
+      <Link href="https://element.eleme.io" type="success" disabled>成功链接</Link>
+      <Link href="https://element.eleme.io" type="warning" :underline="false">警告链接</Link>
+      <Link type="danger" disabled>危险链接</Link>
+      <Link type="info" disabled>信息链接</Link>
     </div>
     <router-view/>
   </div>
 </template>
 <script>
 
+import Link from '@/components/link'
 import Button from '@/components/button'
 export default {
   name: 'App',
   components: {
-    Button
+    Button,
+    Link,
   }
 }
 </script>
